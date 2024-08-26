e1431e3ccc3225143cc9e9b67c8b98e15ee05f7a
thisage6 <thisage6@gmail.com>
2024-08-27 01:37:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const BASE_URL =
+  "https://api.currencyapi.com/v3/latest?apikey=cur_live_3EewVmzRXdgtxUT26G3kGuLaEITv2RgqV3ekcucd";
+
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
+
+for (let select of dropdowns)
+{
+    for (currCode in countryList)
+    {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode==="USD")
+        {
+            newOption.selected = "selected";
+        }
+        else if(select.name === "to" && currCode==="INR")
+            {
+                newOption.selected = "selected";
+            }
+            select.append(newOption);
+    }
+    select.addEventListener("change", (evt) =>
+    {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc= `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src=newSrc;
+}
+
+btn.addEventListener("click" ,(evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal <1) {
+        amtVal = 1;
+        amount.value ="1";
+    }
+
+    const URL = `${BASE_URL}&base_currency=${fromCurr.value}&currencies=${toCurr.value}`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data.data[toCurr.value].value;
+    console.log(rate);
+
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+}
